3061040397f0af0b1c8f0b8813ed77bf40f7bccc
ikapo <63411550+ikapo@users.noreply.github.com>
2023-03-01 14:07:59 +0200

    rewrite test

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -5,9 +5,10 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/_site/customdomain/minimal-repro-vercel-cron.vercel.app/');
+    await res.revalidate('/');
     return res.json({ revalidated: true })
   } catch (err) {
+    console.error(err);
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return res.status(500).send('Error revalidating')
