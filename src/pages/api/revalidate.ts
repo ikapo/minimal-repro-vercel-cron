db2ded22d8d02fe3b45ac9f058ea5f7eab85a8b4
ikapo <63411550+ikapo@users.noreply.github.com>
2023-03-01 17:07:57 +0200

    Try secret

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Check for secret to confirm this is a valid request
+  console.log(req.query.secret);
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
