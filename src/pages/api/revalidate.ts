b939b8142fb394319bdc9f334f801857f9a3805a
ikapo <63411550+ikapo@users.noreply.github.com>
2023-03-01 14:01:25 +0200

    Test revalidate custom domain

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -5,7 +5,7 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/')
+    await res.revalidate('/_site/customdomain/minimal-repro-vercel-cron.vercel.app/');
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
